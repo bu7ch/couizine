cd3869f505dc3116f23be692f46c3f5b42c899d2
Rodolphe <dutchr@live.fr>
2021-06-16 09:55:13 +0200

    Adding basic routing w/ params

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const port = 4000;
 
-
-app.get('/', (req, res) => {
-  res.send('Hello, Universe!');
+app.get("/", (req, res) => {
+  res.send("Hello, Universe!");
+});
+app.get("/contacts", (req, res) => {
+res.send('Les informations ont bien été envoyés avec succès')
+})
+app.get("/items/:legumes", (req, res) => {
+  res.send(req.params.legumes)
 })
 
+// console.log(req.params)
+// console.log(req.body)
+// console.log(req.url)
+// console.log(req.query)
 
-app.listen(port, () => console.log(`Le seveur ecoute sur le port :${port}`))
+app.listen(port, () => console.log(`Le seveur ecoute sur le port :${port}`));
