ac235c8515f16cebc81a1d7fcef7ca96fad24abc
Rodolphe <dutchr@live.fr>
2021-06-16 16:13:26 +0200

    Adding method crUd

--- controllers/subscriberController.js
+++ controllers/subscriberController.js
@@ -17,4 +17,13 @@ exports.postSubscribers = (req, res, next) => {
     if (err) next(err);
     res.render('thanks')
   })
+  exports.editSubscriber = (req, res, next) => {
+    Subscriber.findById(req.params.id, (err, subscriber) => {
+      if (err) console.error(err)
+      Object.assign(subscriber, req.body).save((err, subscriber) => {
+        if (err) console.error(err)
+        res.json({ message : 'Inscription mise à jour', subscriber})
+      })
+    })
+  }
 }
